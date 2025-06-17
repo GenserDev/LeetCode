60a679fd9c1a943c328ce117e144bd4e250e31e9
GenserDev <genserdev@gmail.com>
2025-06-16 21:29:17 -0600

    build: exercise 2667

---
+++ easy/2667. Crear la función Hola mundo/exercise-2667.js
@@ -0,0 +1,28 @@
+// Escribe una función  createHelloWorld. Debe devolver una nueva función que siempre devuelva  "Hello World".
+
+// Ejemplo 1:
+
+// Entrada: args = []
+//  Salida: "Hola mundo"
+//  Explicación:
+// const f = crearHolaMundo();
+// f(); // "Hola mundo"
+
+// La función devuelta por createHelloWorld siempre debe devolver "Hola Mundo".
+// Ejemplo 2:
+
+// Entrada: args = [{},null,42]
+//  Salida: "Hola mundo"
+//  Explicación:
+// const f = crearHolaMundo();
+// f({}, null, 42); // "Hola mundo"
+
+// Se puede pasar cualquier argumento a la función, pero siempre debería devolver "Hola mundo".
+
+function createHelloWorld() {
+    return function() {
+        return "Hello World";
+    }
+}
+
+createHelloWorld()
